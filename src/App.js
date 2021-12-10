32d2f7a6e1f6525ba8888f14c0ae90cecd3638bc
Akashreghukumar <akashakash0211@gmail.com>
2021-12-10 10:12:52 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter,Route,Link} from 'react-router-dom'
+import Home from './pages/home';
+import AdminLogin from './pages/admin';
+import StudentLogin from './pages/student';
+import AboutUs from './pages/aboutus';
+import Contact from './pages/contact';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>React Router App</h1>
+      <BrowserRouter>
+
+      <div className="link">
+      <Link to='/'>Home</Link>
+      <Link to='/adminlogin' >Admin Login</Link>
+      <Link to='/studentlogin' >Student Login</Link>
+<Link to='/aboutus'>About us</Link>
+<Link to='/contact'>Contact</Link>
+
+
+      </div>
+
+      <Route path='/' component={Home} exact/>
+      <Route path="/adminlogin" component={AdminLogin}/>
+      <Route path="/studentlogin" component={StudentLogin}/>
+      <Route path="/aboutus" component={AboutUs}/>
+      <Route path='/contact' component={Contact}/>
+      
+     
+      </BrowserRouter>
+    
     </div>
   );
 }
